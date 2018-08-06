78e9f9e0a7bd6fd491b3b926e5be4f402d610e81
Brian Kasper <bkasperr@gmail.com>
2018-08-06 00:22:46 -0400

    Hide API Key

--- Dollar Pizza Finder/AppDelegate.swift
+++ Dollar Pizza Finder/AppDelegate.swift
@@ -15,7 +15,7 @@ import GooglePlaces
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    let apiKey = "***REMOVED***"
+    let apiKey = ""
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
